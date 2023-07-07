85267b7f468406e7ce61bc3539b581866a0e2657
Matheus Calixto <matheuscalixto8@gmail.com>
2023-07-07 15:02:00 -0300

    feat(users.ts): created user service

---
+++ src/services/users.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { User } from '../types';
+const baseUrl = 'http://localhost:3001/users';
+
+const config = {
+  headers: { Authorization: '' },
+};
+
+const setToken = (newToken: string) => {
+  config.headers.Authorization = `bearer ${newToken}`;
+};
+
+const getById = async (id: string) => {
+  const request = await axios.get(`${baseUrl}/${id}`, config);
+  return request.data;
+};
+
+const update = async (id: string, newObject: User) => {
+  const request = await axios.put(`${baseUrl}/${id}`, newObject, config);
+  return request.data;
+};
+
+export default {
+  update,
+  getById,
+  setToken,
+};
