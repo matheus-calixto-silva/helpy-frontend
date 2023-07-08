f7e6bd8db008273386f5626c1a0532d7b4c438f8
Matheus Calixto <matheuscalixto8@gmail.com>
2023-07-07 21:10:01 -0300

    feat(routes.tsx): added AuthProvider, login and profile routes to app router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,24 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { AuthProvider } from './contexts/AuthProvider/AuthProvider';
 
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+import Profile from './components/Profile/Profile';
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/profile'
+            element={<ProtectedRoute><Profile/></ProtectedRoute>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 };
 
