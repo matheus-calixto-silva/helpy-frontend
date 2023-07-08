54736544616921d93b80a6527729a565c01b8682
Matheus Calixto <matheuscalixto8@gmail.com>
2023-07-07 21:04:18 -0300

    feat(components/protectedroute): created ProtectedRoute component

---
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import { useAuth } from '../../contexts/AuthProvider/useAuth';
+
+const ProtectedLayout = ({ children }: { children: JSX.Element }) => {
+  const auth = useAuth();
+  console.log(auth);
+
+  if (!auth.username) {
+    return <h1>You don&lsquo;t have access</h1>;
+  }
+
+  return children;
+};
+
+export default ProtectedLayout;
