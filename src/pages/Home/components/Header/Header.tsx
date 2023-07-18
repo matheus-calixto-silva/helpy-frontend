26a8bc79922996821fab1b6b072f6518d73dffc1
Matheus Calixto <matheuscalixto8@gmail.com>
2023-07-17 21:43:44 -0300

    refactor(home/header): refactored header structure

--- src/pages/Home/components/Header/Header.tsx
+++ src/pages/Home/components/Header/Header.tsx
@@ -1,19 +1,24 @@
-import ButtonPrimary from '../ButtonPrimary/ButtonPrimary';
-import ButtonSecondary from '../ButtonSecondary/ButtonSecondary';
+import useNavigation from '../../../../navigate';
 
-import Logo from '../../../../assets/logo-red.svg';
+import Logo from '../../../../assets/logo-white.svg';
 
 import styles from './Header.module.css';
 
 const HomeHeader = () => {
+  const navigate = useNavigation();
+
+  function handleClick() {
+    navigate('/login'); // Redirect to the "/login" route
+  }
+
   return (
     <nav className={styles.navbar}>
       <div>
         <img className={styles.logo} src={Logo} alt="Helpy logo" />
       </div>
       <div className={styles.buttons}>
-        <ButtonPrimary>Entrar</ButtonPrimary>
-        <ButtonSecondary>Cadastre-se</ButtonSecondary>
+        <button className={`b3 ${styles.button_primary}`} onClick={handleClick}>Entrar</button>
+        <button className={`b3 ${styles.button_secondary}`}>Cadastre-se</button>
       </div>
     </nav>
   );
