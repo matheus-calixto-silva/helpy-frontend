5ba35a622a0c2295fd5820c1a60cc89b56401400
Matheus Calixto <matheuscalixto8@gmail.com>
2023-07-17 21:42:14 -0300

    refactor(home/about): rewrited About title

--- src/pages/Home/components/About/About.tsx
+++ src/pages/Home/components/About/About.tsx
@@ -10,7 +10,7 @@ const About = () => {
           <img src={AboutPhoto} className={styles.about_image} alt="Crianças" />
         </div>
         <div className={styles.about_content}>
-          <h2>Nossa maior ambição está em ajudar pessoas</h2>
+          <h2>Ambição em ajudar pessoas</h2>
           <p className='b2'>Agimos com solidariedade e empatia, colocando o bem-estar das pessoas em primeiro lugar. Queremos deixar um legado de esperança, contribuindo para uma sociedade justa e inclusiva.</p>
           <p className='b2'><a href="#">Saiba Mais</a></p>
         </div>
