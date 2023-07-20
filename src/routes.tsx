54f7251e1396276bd1ada1551902b1fd00ca5a20
Matheus Calixto <matheuscalixto8@gmail.com>
2023-07-19 21:02:21 -0300

    refactor(routes.tsx): refactored the use of Profile to User in routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import { AuthProvider } from './contexts/AuthProvider/AuthProvider';
 
 import Home from './pages/Home/Home';
 import Login from './components/Login/Login';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
-import Profile from './components/Profile/Profile';
+import User from './components/User/User';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          <Route path='/profile' element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-          <Route path="/" element={<Home />} />
+          <Route path='/' element={<ProtectedRoute><User /></ProtectedRoute>} />
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </AuthProvider>
