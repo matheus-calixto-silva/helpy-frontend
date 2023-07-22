8d117fbd2cd9c20fc13686f26e1cc58334d2cc83
Matheus Calixto <matheuscalixto8@gmail.com>
2023-07-22 16:37:37 -0300

    feat(components/notfound): created not found component

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import Button from '../Button/Button';
+import styles from './NotFound.module.css';
+
+const NotFound = () => {
+  return (
+    <div className={styles.not_found}>
+      <h1>404</h1>
+      <h4>Desculpe,página não encontrada</h4>
+      <Link to={'/home'} className={'b3'}>
+        <Button>Voltar para Home</Button>
+      </Link>
+    </div>);
+};
+
+export default NotFound;
