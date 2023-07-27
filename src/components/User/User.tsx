9a7e1558088d3dd54a182dfbdbc84c5e0d6419bf
Matheus Calixto <matheuscalixto8@gmail.com>
2023-07-27 12:34:12 -0300

    feat(components/user): created User component

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,18 +1,33 @@
+import { Route, Routes } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthProvider/useAuth';
+
+import UserNavBar from '../UserNavBar/UserNavBar';
 
 import Admin from '../Admin/Admin';
 import Ong from '../Ong/Ong';
 import Volunteer from '../Volunteer/Volunteer';
+import NotFound from '../NotFound/NotFound';
+import MyEvents from '../MyEvents/MyEvents';
+import CreateEvent from '../CreateEvent/CreateEvent';
 
 const Profile = () => {
   const auth = useAuth();
   return (
-    <div>
-      <h1>Profile</h1>
+    <section>
+      <UserNavBar />
       {auth.role === 'user' && <Volunteer />}
-      {auth.role === 'ong' && <Ong />}
+      {auth.role === 'ong' && (
+        <Routes>
+          <Route path='/' element={<Ong />} />
+          <Route path='/criar-evento' element={<CreateEvent />} />
+          <Route path='/meus-eventos' element={<MyEvents />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      )}
       {auth.role === 'admin' && <Admin />}
-    </div>);
+      <button onClick={auth.handleLogout}>logout</button>
+    </section>
+  );
 };
 
 export default Profile;
