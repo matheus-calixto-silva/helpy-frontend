40f3cfdc8dc899a2e4d33fc665749778af286091
Matheus Calixto <matheuscalixto8@gmail.com>
2023-07-27 10:41:21 -0300

    feat(services/ibge.ts): created the service responsible for connecting with the IBGE api

---
+++ src/services/ibge.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const baseUrl = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+
+const getAllUfs = async () => {
+  const request = await axios.get(baseUrl);
+  return request.data;
+};
+
+const getAllCities= async (uf: string) => {
+  const request = await axios.get(`${baseUrl}/${uf}/municipios`);
+  return request.data;
+};
+
+
+export default {
+  getAllUfs,
+  getAllCities
+};
