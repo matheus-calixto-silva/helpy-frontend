5ab5e05ab46cbfaa969df4df4d1efc1e1e58f9e9
Matheus Calixto <matheuscalixto8@gmail.com>
2023-08-03 14:17:25 -0300

    feat(components/dropzone): created dropzone component

---
+++ src/components/Dropzone/Dropzone.tsx
@@ -0,0 +1,46 @@
+import { FC, useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+
+import styles from './Dropzone.module.css';
+
+interface Props {
+  onFileUploaded: (file: File) => void;
+}
+
+const Dropzone: FC<Props> = ({ onFileUploaded }) => {
+  const [selectedFileUrl, setSelectedFileUrl] = useState('');
+
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      const file = acceptedFiles[0];
+      const fileurl = URL.createObjectURL(file);
+
+      setSelectedFileUrl(fileurl);
+      onFileUploaded(file);
+    },
+    [onFileUploaded]
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: {
+      'image/png': ['.png'],
+      'image/jpg': ['.jpg'],
+      'image/jpeg': ['.jpeg'],
+    },
+  });
+
+  return (
+    <div className={styles.dropzone} {...getRootProps()}>
+      <input {...getInputProps()} name='photo' accept="image/*" />
+
+      {selectedFileUrl ? (
+        <img src={selectedFileUrl} alt="Event thumbnail" />
+      ) : (
+        <p className='b3'>Adicione uma imagem do evento</p>
+      )}
+    </div>
+  );
+};
+
+export default Dropzone;
