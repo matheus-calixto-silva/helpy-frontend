7e86fe0e0f6ff0c41197754a04aacbbfd0adb394
Matheus Calixto <matheuscalixto8@gmail.com>
2023-10-14 14:35:09 -0300

    refactor(components/user): refactored ongs routes to other component

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,15 +1,10 @@
-import { Route, Routes } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthProvider/useAuth';
 
 import UserNavBar from '../UserNavBar/UserNavBar';
 
 import Admin from '../Admin/Admin';
-import Ong from '../Ong/Ong';
 import Volunteer from '../Volunteer/Volunteer';
-import NotFound from '../NotFound/NotFound';
-import MyEvents from '../MyEvents/MyEvents';
-import CreateEvent from '../CreateEvent/CreateEvent';
-import EventDetails from '../EventDetails/EventDetails';
+import OngRoutes from '../OngRoutes/OngRoutes';
 
 const Profile = () => {
   const auth = useAuth();
@@ -17,15 +12,7 @@ const Profile = () => {
     <section>
       <UserNavBar />
       {auth.role === 'user' && <Volunteer />}
-      {auth.role === 'ong' && (
-        <Routes>
-          <Route path='/' element={<Ong />} />
-          <Route path='/criar-evento' element={<CreateEvent />} />
-          <Route path='/meus-eventos' element={<MyEvents />} />
-          <Route path='/meus-eventos/:eventId' element={<EventDetails />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      )}
+      {auth.role === 'ong' && <OngRoutes />}
       {auth.role === 'admin' && <Admin />}
     </section>
   );
