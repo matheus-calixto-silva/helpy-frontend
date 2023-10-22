cb66b3e5a009b9d8e297ee8f52a9fc0c956afa51
Matheus Calixto <matheuscalixto8@gmail.com>
2023-10-21 21:01:50 -0300

    feat(types.ts): updated types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,45 @@
+import { ChangeEvent, FocusEvent } from 'react';
+export interface Admin {
+  _id: string;
+  firstname: string;
+  lastname: string;
+  username: string;
+  password?: string;
+  passwordHash?: string;
+  email: string;
+  phone: string;
+  profilePic: string;
+  created_at: number;
+  updated_at: number;
+  role: role;
+}
+
+export interface Ong extends Admin {
+  address: string;
+  cnpj: string;
+  maxEvents: number;
+  events: Event[]
+}
+
+export interface User extends Admin {
+  skills: Skill[];
+}
+export interface Event {
+  _id?: string;
+  address: Address;
+  name: string;
+  description: string;
+  date: Date;
+  requiredSkills: Skill[];
+  maxVolunteers: number;
+  volunteers: User[];
+  eventPic: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+type role = 'admin' | 'user' | 'ong';
+
 export interface Category {
   name: string;
   description: string;
@@ -6,37 +48,73 @@ export interface Category {
 export interface Skill {
   name: string;
   category: Category;
-  id: string;
+  _id: string;
 }
-export interface Skills {
-  skill: Skill;
-  _id:   string;
-}
-export interface User {
-  firstname:  string;
-  lastname:   string;
-  username:   string;
-  email:      string;
-  phone:      string;
-  profilePic: string;
-  skills:     Skills[];
-  created_at: Date;
-  updated_at: Date;
-  id:         string;
-}
-
 export interface IUser {
   username?: string;
   password?: string;
   token?: string;
   role?: string;
+  id?: string;
 }
-
 export interface IContext extends IUser {
   handleLogin: (username: string, password: string) => Promise<void>;
   handleLogout: () => void
 }
-
 export interface IAuthProvider {
   children: JSX.Element;
+}
+
+export interface InputProps {
+  label: string;
+  type: string;
+  name: string;
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (event: FocusEvent<HTMLInputElement>) => void; // Make onBlur optional
+  error?: string;
+}
+
+export interface Address {
+  street: string;
+  city: string;
+  uf: string;
+  latitude: string;
+  longitude: string;
+}
+
+export interface City {
+  id: number;
+  nome: string;
+  microrregiao: Microrregiao;
+}
+
+export interface Microrregiao {
+  id: number;
+  nome: string;
+  mesorregiao: Mesorregiao;
+}
+
+export interface Mesorregiao {
+  id: number;
+  nome: string;
+  UF: Uf;
+}
+
+export interface Uf {
+  id: number;
+  sigla: string;
+  nome: string;
+  regiao?: Regiao;
+}
+
+export interface Regiao {
+  id: number;
+  sigla: string;
+  nome: string;
+}
+
+export interface DatePickerComponentProps {
+  selectedDate: Date | null;
+  handleDateChange: (date: Date | null) => void;
 }
