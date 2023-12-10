6be77d8e4d482aaf023ca926a09a2e770d79ab1a
Matheus Calixto <matheuscalixto8@gmail.com>
2023-12-10 13:05:12 -0300

    refactor(home/components/hero): adjustments to the home page hero CSS

--- src/pages/Home/components/Hero/Hero.tsx
+++ src/pages/Home/components/Hero/Hero.tsx
@@ -5,14 +5,16 @@ import HeroPhoto from '../../../../assets/charity-hero.png';
 const Hero = () => {
   return (
     <section className={styles.hero}>
-      <div className={styles.hero_content}>
-        <h2>Transforme vidas através de suas ações</h2>
-        <p className='b1'>Seja um agente de transformação e veja o poder das suas ações</p>
-        <p className='b2'><a href="#">Saiba Mais</a></p>
-      </div>
-      <div className={styles.hero_image_wrapper}>
-        <img src={HeroPhoto} className={styles.hero_image} alt="Crianças" />
-      </div>
+      <section className={styles.container}>
+        <div className={styles.hero_content}>
+          <h2>Transforme vidas através de suas ações</h2>
+          <p className='b1'>Seja um agente de transformação e veja o poder das suas ações</p>
+          <p className='b2'><a href="#">Saiba Mais</a></p>
+        </div>
+        <div className={styles.hero_image_wrapper}>
+          <img src={HeroPhoto} className={styles.hero_image} alt="Crianças" />
+        </div>
+      </section>
     </section>
   );
 };
