6fb31ac32ab8020948012df7c93c644dda5a265c
Matheus Calixto <matheuscalixto8@gmail.com>
2023-12-10 13:15:49 -0300

    refactor(home/components/about): adjustments to the home page's about CSS

--- src/pages/Home/components/About/About.tsx
+++ src/pages/Home/components/About/About.tsx
@@ -4,19 +4,21 @@ import AboutPhoto from '../../../../assets/charity-about.png';
 
 const About = () => {
   return (
-    <div className={styles.container}>
-      <section className={styles.about}>
-        <div className={styles.about_image_wrapper}>
-          <img src={AboutPhoto} className={styles.about_image} alt="Crianças" />
+    <section className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.about}>
+          <div className={styles.about_image_wrapper}>
+            <img src={AboutPhoto} className={styles.about_image} alt="Crianças" />
+          </div>
+          <div className={styles.about_content}>
+            <h2>Ambição em ajudar pessoas</h2>
+            <p className='b2'>Agimos com solidariedade e empatia, colocando o bem-estar das pessoas em primeiro lugar. Queremos deixar um legado de esperança, contribuindo para uma sociedade justa e inclusiva.</p>
+            <p className='b2'><a href="#">Saiba Mais</a></p>
+          </div>
         </div>
-        <div className={styles.about_content}>
-          <h2>Ambição em ajudar pessoas</h2>
-          <p className='b2'>Agimos com solidariedade e empatia, colocando o bem-estar das pessoas em primeiro lugar. Queremos deixar um legado de esperança, contribuindo para uma sociedade justa e inclusiva.</p>
-          <p className='b2'><a href="#">Saiba Mais</a></p>
-        </div>
-      </section>
-      <div className={styles.background_red}></div>
-    </div>
+        <div className={styles.background_red}></div>
+      </div>
+    </section>
   );
 };
 
