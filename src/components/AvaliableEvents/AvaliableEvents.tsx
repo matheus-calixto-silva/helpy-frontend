38036c1e6be0b5803415a6a66f88696e059cd599
Matheus Calixto <matheuscalixto8@gmail.com>
2024-03-06 11:56:14 -0300

    feat(components/avaliableevents): created AvaliableEvents component

---
+++ src/components/AvaliableEvents/AvaliableEvents.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef, useState } from 'react';
+
+import AvaliableEventCard from '../AvaliableEventCard/AvaliableEventCard';
+
+import styles from './AvaliableEvents.module.css';
+
+import { Event } from '../../types';
+
+import { motion } from 'framer-motion';
+
+const AvaliableEvents = ({ events }: { events: Event[] }) => {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    if (carousel.current) {
+      setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    }
+  }, [width]);
+
+  return (
+    <motion.section 
+      ref={carousel} 
+      className={styles.carousel} 
+      drag="x" whileTap={{ cursor: 'grabbing' }} 
+      dragConstraints={{right: 0, left: -width}}>
+      <motion.div className={styles.inner}>
+        {events ? events.map((event) => (
+          <AvaliableEventCard event={event} key={event._id} />
+        )) : 'nenhum evento encontrado'}
+      </motion.div>
+    </motion.section>
+  );
+};
+
+export default AvaliableEvents;
