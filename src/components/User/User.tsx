d07871010644a13fb7d3e4c7eafa05b9f0719a22
Matheus Calixto <matheuscalixto8@gmail.com>
2024-03-06 11:55:01 -0300

    refactor(components/user): updated User component

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -3,7 +3,7 @@ import { useAuth } from '../../contexts/AuthProvider/useAuth';
 import UserNavBar from '../UserNavBar/UserNavBar';
 
 import Admin from '../Admin/Admin';
-import Volunteer from '../Volunteer/Volunteer';
+import VolunteerRoutes from '../VolunteerRoutes/VolunteerRoutes';
 import OngRoutes from '../OngRoutes/OngRoutes';
 
 const Profile = () => {
@@ -11,7 +11,7 @@ const Profile = () => {
   return (
     <section>
       <UserNavBar />
-      {auth.role === 'user' && <Volunteer />}
+      {auth.role === 'user' && <VolunteerRoutes />}
       {auth.role === 'ong' && <OngRoutes />}
       {auth.role === 'admin' && <Admin />}
     </section>
