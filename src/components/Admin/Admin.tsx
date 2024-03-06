2c6f67a094b69a527b39e3b4904f0bc3d84e573e
Matheus Calixto <matheuscalixto8@gmail.com>
2024-03-06 11:58:24 -0300

    feat(components/admin): created Admin component

---
+++ src/components/Admin/Admin.tsx
@@ -0,0 +1,12 @@
+import { useAuth } from '../../contexts/AuthProvider/useAuth';
+
+const Admin = () => {
+  const auth = useAuth();
+  return (
+    <div>
+      <h1>Admin</h1>
+      <button onClick={auth.handleLogout}>logout</button>
+    </div>);
+};
+
+export default Admin;
