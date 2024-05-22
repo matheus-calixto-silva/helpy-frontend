d4d2738831d70dc96f8c39be223e04af40bb04f7
Matheus Calixto <matheuscalixto8@gmail.com>
2024-05-22 15:19:13 -0300

    chore: added aditional config to path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
+import path from 'node:path';
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@views': path.resolve(__dirname, 'src', 'views'),
+      '@app': path.resolve(__dirname, 'src', 'app'),
     },
   },
   plugins: [react()],
